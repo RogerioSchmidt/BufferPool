b9f0aeb01aa489ae84ce533ef7d9c1bf5eee4060
NatanJMai <natan.mai@hotmail.com>
2014-09-30 17:56:02 -0300

    Organizei na pasta Desenv, depois se precisar mudamos.

---
+++ Desenv/buffNRR.c
@@ -0,0 +1,18 @@
+/*/////////////////////////////////////////////////////////////////////////////////*
+//  Alunos: Natan J. Mai, Ricardo Zanuzzo e Rogério Torchelsen                    //
+//  Disciplina: Banco de Dados II                                                 //      
+//  Curso: Ciência da Computação                                                  //      
+//  Universidade Federal da Fronteira Sul                                         //      
+//                                                                                //      
+//                                                                                //      
+//                                                                                //      
+//////////////////////////////////////////////////////////////////////////////////*/
+
+
+
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
