5b176f6c385657e0f324ee9e99a54ac135807caa
RogerioSchmidt <rogelho.schmidt@gmail.com>
2014-10-04 18:46:07 -0300

    atualizado imprimi

--- Testes/imprimi.c
+++ Testes/imprimi.c
@@ -1,1 +1,7 @@
-
+int imprimiTab(int opcao){
+	FILE *carro, *fabricante, *proprietario, *modelo;
+	if(opcao==1){
+	carro= fopen("carro.dat", "rb");
+	if(arquivo == NULL){
+		printf("Nãop foi possível abrir o arquivo Meta.dat\n");
+		exit(1);
