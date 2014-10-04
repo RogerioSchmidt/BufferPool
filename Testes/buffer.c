6f681c12bb0b803c1182457e335d1d4ded83f172
zanuzzorz <zanuzzorz@gmail.com>
2014-10-04 17:16:57 -0300

    Atualização do arquivo buffer para inserir valores

--- Testes/buffer.c
+++ Testes/buffer.c
@@ -1,4 +1,4 @@
-#include "buffend.c"
+#include "buffend.h"
 
 
 int main(){
@@ -13,43 +13,46 @@ int main(){
     table  *tab[nrTabelas];
     column *colunas = NULL;
 
-    /* Cria nrTabelas tabelas. */    
+		/* Cria nrTabelas tabelas. */    
+		tab[0] = iniciaTabela("Carro");    
+		tab[1] = iniciaTabela("Modelo");    
+		tab[2] = iniciaTabela("Fabricante");    
+		tab[3] = iniciaTabela("Proprietário");    
 
-    tab[0] = iniciaTabela("Carro");    
-    tab[1] = iniciaTabela("Modelo");    
-    tab[2] = iniciaTabela("Fabricante");    
-    tab[3] = iniciaTabela("Proprietário");    
+		for(indc = 0; indc < nrTabelas; indc++){
+			if(tab[indc] == ERRO_NOME_TABELA_INVALIDO){
+				printf("Erro: na função iniciaTabela(). Nome da tabela já existente.\n");
+				return 0;   
+			}
+		}
 
-    for(indc = 0; indc < nrTabelas; indc++){
-        if(tab[indc] == ERRO_NOME_TABELA_INVALIDO){
-            printf("Erro: na função iniciaTabela(). Nome da tabela já existente.\n");
-            return 0;
-        }
-    }
+		//Carro
+		tab[0] = adicionaCampo(tab[0], "Rnbr", 'S', 20);
+		tab[0] = adicionaCampo(tab[0], "Kms", 'I', (sizeof(int)));
+		tab[0] = adicionaCampo(tab[0], "DataCompra", 'S', 10);
+		tab[0] = adicionaCampo(tab[0], "Ano", 'I', (sizeof(int)));
+		tab[0] = adicionaCampo(tab[0], "Modelo", 'I', (sizeof(int)));
+		tab[0] = adicionaCampo(tab[0], "Motor", 'D', (sizeof(double)));
+		erro = finalizaTabela(tab[0]);
+		
 
-    //Carro
-    tab[0] = adicionaCampo(tab[0], "Rnbr"      , 'S' , 3);
-    tab[0] = adicionaCampo(tab[0], "Kms"       , 'I' , (sizeof(int)));
-    tab[0] = adicionaCampo(tab[0], "DataCompra", 'S' , 5);
-    tab[0] = adicionaCampo(tab[0], "Ano"       , 'S' , (sizeof(int)));
-    tab[0] = adicionaCampo(tab[0], "Modelo"    , 'I' , (sizeof(int)));
-    tab[0] = adicionaCampo(tab[0], "Motor"     , 'S' , 3);
+		//Modelo
+		tab[1] = adicionaCampo(tab[1], "Modelo"    , 'S', 5);
+		tab[1] = adicionaCampo(tab[1], "NomeMod"   , 'S', 10);
+		tab[1] = adicionaCampo(tab[1], "Fabricante", 'S', 5);
+		erro = finalizaTabela(tab[1]);
 
-    //Modelo
-    tab[1] = adicionaCampo(tab[1], "Modelo"    , 'S', 5);
-    tab[1] = adicionaCampo(tab[1], "NomeMod"   , 'S', 10);
-    tab[1] = adicionaCampo(tab[1], "Fabricante", 'S', 5);
+		//Fabricante
+		tab[2] = adicionaCampo(tab[2], "FatCod"    , 'S', 5);
+		tab[2] = adicionaCampo(tab[2], "NomeFat"   , 'S', 15);
+		erro = finalizaTabela(tab[2]);
 
-    //Fabricante
-    tab[2] = adicionaCampo(tab[2], "FatCod"    , 'S', 5);
-    tab[2] = adicionaCampo(tab[2], "NomeFat"   , 'S', 15);
-    
-
-    //Proprietário
-    tab[3] = adicionaCampo(tab[3], "Rg"        , 'S', 10);    
-    tab[3] = adicionaCampo(tab[3], "Nome"      , 'S', 15);    
-    tab[3] = adicionaCampo(tab[3], "DataNasc"  , 'S', 8);    
-    tab[3] = adicionaCampo(tab[3], "Email"     , 'S', 20);   
+		//Proprietário
+		tab[3] = adicionaCampo(tab[3], "Rg"        , 'S', 10);    
+		tab[3] = adicionaCampo(tab[3], "Nome"      , 'S', 15);    
+		tab[3] = adicionaCampo(tab[3], "DataNasc"  , 'S', 8);    
+		tab[3] = adicionaCampo(tab[3], "Email"     , 'S', 20);    
+		erro = finalizaTabela(tab[3]); 
     
 	
 	
@@ -88,7 +91,7 @@ int main(){
 					colunas = insereValor(colunas, "Modelo", codM);
 					colunas = insereValor(colunas, "Motor", motor);
 
-					erro = finalizaInsert("Carro", colunas);		
+					erro = finalizaInsert("Carro", colunas);	
 					
 					break;
 				case 2:
@@ -132,7 +135,7 @@ int main(){
 					
 					colunas = insereValor(colunas, "Rg", rg);
 					colunas = insereValor(colunas, "Nome", nome);
-					colunas = insereValor(colunas, "DataNasc", dataN);
+					colunas = insereValor(colunas, "DataNasc", dataN);  
 					colunas = insereValor(colunas, "Email", email);
 
 					erro = finalizaInsert("Proprietário", colunas);		
@@ -152,5 +155,89 @@ int main(){
 		}
 	
 	
+	/*
+	printf("Deseja Imprimir as tableas criadas (0 = não 1, = sim)? \n");
+	scanf("%d",&imprime);
+	
+	if(imprime==1){
+		if(erro != SUCCESS){
+			printf("Erro %d: na função finalizaInsert()\n", erro);
+			return 0;
+		}
+
+		struct fs_objects objeto = leObjeto(tabela);	
+		
+		tp_table *esquema = leSchema(objeto);
+
+		if(esquema == ERRO_ABRIR_ESQUEMA){
+			printf("Erro ao criar o esquema.\n");
+			return 0;
+		}
+
+		tp_buffer *bufferpoll = initbuffer();
+
+		if(bufferpoll == ERRO_DE_ALOCACAO){
+			printf("Erro ao alocar memória para o buffer.\n");
+			return 0;
+		}
+
+		erro = colocaTuplaBuffer(bufferpoll, 0, esquema, objeto);
+
+		if(erro != SUCCESS){
+			printf("Erro %d: na função colocaTuplaBuffer().\n", erro);
+			return 0;
+		}
+		
+		erro = colocaTuplaBuffer(bufferpoll, 1, esquema, objeto);
+
+		if(erro != SUCCESS){
+			printf("Erro %d: na função colocaTuplaBuffer().\n", erro);
+			return 0;
+		}
+		
+		erro = colocaTuplaBuffer(bufferpoll, 2, esquema, objeto);
+
+		if(erro != SUCCESS){
+			printf("Erro %d: na função colocaTuplaBuffer().\n", erro);
+			return 0;
+		}
+		
+		//column *tuplaE = excluirTuplaBuffer(bufferpoll, esquema, objeto, 0, 2); //pg, tupla
+		column *pagina = getPage(bufferpoll, esquema, objeto, 0);
+
+		if(tuplaE == ERRO_PARAMETRO){
+			printf("Erro, na função excluirTuplaBuffer(), problemas no parametro.\n");
+			return 0;
+		}
+		if(pagina == ERRO_PARAMETRO){
+			printf("Erro, na função getPage(), problemas no parametro.\n");
+			return 0;
+		}
+		
+		// PARA IMPRIMIR PÁGINA ---------------------------------------
+		//-------------------------------------------------------------
+		printf("\nPágina armazenada na estrutura column *pagina.\n");
+		for(j=0; j < objeto.qtdCampos*bufferpoll[0].nrec; j++){
+			
+			if(pagina[j].tipoCampo == 'S')
+				printf("%s: %s ", pagina[j].nomeCampo,pagina[j].valorCampo);
+			else if(pagina[j].tipoCampo == 'I'){
+				int *n = (int *)&pagina[j].valorCampo[0];
+				printf("%s: %d ",pagina[j].nomeCampo, *n);
+			}
+			else if(pagina[j].tipoCampo == 'C'){
+				printf("%s: %c ",pagina[j].nomeCampo, pagina[j].valorCampo[0]);
+			}
+			else if(pagina[j].tipoCampo == 'D'){
+				double *n = (double *)&pagina[j].valorCampo[0];
+				printf("%s: %f ",pagina[j].nomeCampo, *n);
+			}
+			printf("\n");
+		}
+		printf("\n\n");
+	}
+	
+	//-------------------------------------------------------------
+*/
 	return 0;
 }		
